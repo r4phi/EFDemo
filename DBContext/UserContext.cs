84b8646d77264e958463ba12082967e6619d9f75
raphi <raphi.kapeller@gmx.at>
2022-01-14 12:53:25 +0100

    end of lesson

---
+++ DBContext/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo.DBContext
+{
+    class UserContext :System.Data.Entity.DbContext
+    {
+    }
+}
